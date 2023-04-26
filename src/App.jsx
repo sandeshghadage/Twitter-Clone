0b3e4a7bb30f139f2ff85a0dc30a6a9317ebab62
therohanmehta <rohandhn786@gmail.com>
2023-04-26 22:44:43 +0530

    this commit is about the login part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes,Navigate } from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage";
 import RegisterPage from "./pages/registerPage/RegisterPage";
-import LoginPage from "./pages/loginPage/loginPage";
+import LoginPage from "./pages/loginPage/LoginPage";
 function App() {
+
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Navigate to={"/home"} />}></Route>
+      <Route path="/home" element={<HomePage />} />
       <Route path="/signin" element={<LoginPage />} />
       <Route path="/signup" element={<RegisterPage />} />
     </Routes>
