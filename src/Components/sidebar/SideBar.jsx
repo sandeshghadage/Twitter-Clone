a81a33e644e722572c02c6b035dde2d8991ed06e
sandeshghadage <sandeshghadage3@gmail.com>
2023-05-01 09:14:12 +0530

    liked and dynamic feed implemented

--- src/Components/sidebar/SideBar.jsx
+++ src/Components/sidebar/SideBar.jsx
@@ -1,13 +1,37 @@
 import Icons from "../../Datas/SidebarData";
 import { Button, Dialog, DialogContent, DialogTitle } from "@mui/material";
 import style from "./SideBar.module.css";
-import { ImCross } from "react-icons/im";
-import { useState } from "react";
+import { CgClose } from "react-icons/cg";
 import { FaFeatherAlt } from "react-icons/fa";
+import { BsFileImage } from "react-icons/bs";
 import UsersName from "../userName/UsersName";
+import { AiOutlineFileGif, AiOutlineSchedule } from "react-icons/ai";
+import { BiPoll, BiChevronDown } from "react-icons/bi";
+import { HiOutlineEmojiHappy } from "react-icons/hi";
+import { CiLocationOn } from "react-icons/ci";
+import { RiEarthFill } from "react-icons/ri";
+import { useRecoilState } from "recoil";
+import { tweetMsgAtom, tweetAtom, tweetDialog } from "../../recoil/TweetAtom";
 
 export default function SideBar() {
-  const [isdialog, setIsDialog] = useState(false);
+  const [isdialog, setIsDialog] = useRecoilState(tweetDialog);
+  const [tweetMsg, setTweetMsg] = useRecoilState(tweetMsgAtom);
+  const [tweet, setTweet] = useRecoilState(tweetAtom);
+
+  const dummyData = {
+    id: Date.now(),
+    content: "",
+    createdAt: Date.now(),
+    image: `https://picsum.photos/1000/500?q=${Date.now()}`,
+    tweetedBy: {
+      id: "a2b9f2ce-a4bf-45bd-a545-5ee996ffa451",
+      name: "You",
+    },
+    likeCount: 0,
+    commentCount: 0,
+    reTweetsCount: 0,
+    isLiked: false,
+  };
 
   const buttonStyle = {
     width: "13rem",
@@ -20,6 +44,14 @@ export default function SideBar() {
       display: "none",
     },
   };
+
+  function handleTweet() {
+    const newDummy = { ...dummyData };
+    newDummy.content = tweetMsg;
+    setTweet([newDummy, ...tweet]);
+    setIsDialog(false);
+  }
+
   return (
     <>
       <div className={style.Sidebar}>
@@ -44,14 +76,112 @@ export default function SideBar() {
       <div className={style.UsersName}>
         <UsersName />
       </div>
-      <Dialog open={isdialog}>
+      {/* <TweetDialog onClick={handleTweet} /> */}
+      <Dialog
+        PaperProps={{
+          style: { borderRadius: 15 },
+          sx: {
+            width: "100%",
+            minHeight: 300,
+          },
+        }}
+        open={isdialog}
+      >
         <DialogTitle>
-          <span onClick={() => setIsDialog(false)}>
-            <ImCross />
-          </span>
+          <CgClose
+            onClick={() => setIsDialog(false)}
+            style={{ cursor: "pointer" }}
+          />
         </DialogTitle>
-        <DialogContent>dtyfyguujgyhgvyjhfdgtygjyfuk</DialogContent>
+        <DialogContent>
+          <div id={style.mainBox}>
+            <div id={style.content}>
+              <div style={{ display: "flex", gap: "15px" }}>
+                <div id={style.imgBox}>
+                  <img
+                    src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt="profile"
+                    style={{
+                      height: "100%",
+                      width: "100%",
+                      objectFit: "cover",
+                      borderRadius: "50%",
+                    }}
+                  />
+                </div>
+                <div
+                  id={style.feedType}
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "1rem",
+                  }}
+                >
+                  <PublicDropDown />
+                  <input
+                    onChange={(e) => setTweetMsg(e.target.value)}
+                    placeholder="What's happening?"
+                  />
+                </div>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  margin: "100px 0px 0px 64px",
+                  gap: "10px",
+                }}
+              >
+                <div id={style.selectedType}>
+                  {" "}
+                  <RiEarthFill />
+                  Everyone can reply
+                </div>
+                <div id={style.addTweetFoot}>
+                  <div className={style.icons}>
+                    <div>
+                      <BsFileImage className={style.icon} />
+                    </div>
+                    <div>
+                      <AiOutlineFileGif className={style.icon} />
+                    </div>
+                    <div>
+                      <BiPoll className={style.icon} />
+                    </div>
+                    <div>
+                      <HiOutlineEmojiHappy className={style.icon} />
+                    </div>
+                    <div>
+                      <AiOutlineSchedule className={style.icon} />
+                    </div>
+                    <div>
+                      <CiLocationOn className={style.icon} />
+                    </div>
+                  </div>
+                  <TweetButton onClick={handleTweet} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </DialogContent>
       </Dialog>
     </>
+  );
+}
+
+function TweetButton({ onClick }) {
+  return (
+    <button onClick={onClick} id={style.tweetBtn}>
+      Tweet
+    </button>
+  );
+}
+
+function PublicDropDown() {
+  return (
+    <div id={style.feedTypeBtn}>
+      Everyone
+      <BiChevronDown />{" "}
+    </div>
   );
 }
